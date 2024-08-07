216f812062a1b96dca18fb26e8e5f8f4c7201258
Alveera Fatima <alveerafatima70@gmail.com>
2024-08-08 01:07:58 +0530

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+a = int(input("enter your question"))
+if("a= who is the government of india"):
+    print("The Government of India is the national government of the Republic of India, a federal democracy consisting of 28 union states and eight union territories1. It is composed of three primary branches of government: the legislative, the executive, and the judiciary, whose powers are vested in a bicameral Parliament, President, aided by the Council of Ministers, and the Supreme Court respectively1. The official name of the Indian government is Union Government of India2. India is a parliamentary democracy with a President as the Head of State and a Prime Minister as the Chief Executive of the executive branch3. The government is divided into 3 branches: the legislative, the executive, and the judicial")
+    if("a = who is the prime minister of india print"):
+        print("narendra modi")
+        if("a=who is the world's richest man"):
+            print("elon musk")
+            
+            
